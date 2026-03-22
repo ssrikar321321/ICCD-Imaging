f21be187328ca5908d2ba29967d438b07950a0d6
ssrikar321321 <55238962+ssrikar321321@users.noreply.github.com>
2026-03-22 19:54:23 +0100

    Update ICCD.py

--- ICCD.py
+++ ICCD.py
@@ -364,7 +364,7 @@ with st.sidebar:
                 format="%.1f"
             )
 
-        time_units = ["ns", "μs", "ms", "s"]
+        time_units = ["ns", "\u03bcs", "ms", "s"]
         current_unit = st.session_state.time_settings['unit']
         try:
             default_index = time_units.index(current_unit)
